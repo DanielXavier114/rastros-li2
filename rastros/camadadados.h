8c4b9848ef5372d744bfac2260f4fb2cef62d8ee
JoaoGiesteira33 <joaollgiesteira@gmail.com>
2020-03-12 14:20:53 +0000

    Mege correçao

--- rastros/camadadados.h
+++ rastros/camadadados.h
@@ -1,26 +1,5 @@
 #ifndef RASTROS_CAMADADADOS_H
 #define RASTROS_CAMADADADOS_H
-typedef enum {VAZIO, BRANCA, PRETA} CASA;
-typedef struct {int coluna;int linha;} COORDENADA;
-typedef struct {COORDENADA jogador1;COORDENADA jogador2;} JOGADA;
-typedef JOGADA JOGADAS[32];
-typedef struct {
-    CASA tab[8][8];
-    COORDENADA ultima_jogada;
-    JOGADAS jogadas;
-    int num_jogadas;
-    int jogador_atual;
-} ESTADO;
-
-<<<<<<< HEAD
-ESTADO *inicializar_estado();
-
-int obter_jogador_atual(ESTADO *estado);
-
-int obter_numero_de_jogadas(ESTADO *estado);
-
-CASA obter_estado_casa(ESTADO *e, COORDENADA c);
-=======
 typedef enum
 {
     VAZIO,
@@ -48,9 +27,10 @@ typedef struct
 } ESTADO;
 
 ESTADO *inicializar_estado();
-int obter_jogador_atual(ESTADO *estado);
-int obter_numero_de_jogadas(ESTADO *estado);
-CASA obter_estado_casa(ESTADO *e, COORDENADA c);
 
->>>>>>> dc400e3f727a6ce428e8398cf088ec3bf13cdc8e
+int obter_jogador_atual(ESTADO *estado);
+
+int obter_numero_de_jogadas(ESTADO *estado);
+
+CASA obter_estado_casa(ESTADO *e, COORDENADA c);
 #endif //RASTROS_CAMADADADOS_H
